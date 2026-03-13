68686e0a179e56c4e63c54aeded1fbf073fec53d
Muvaffak Onus <me@muvaf.com>
2026-03-13 04:29:59 +0300

    packagejson: re-export limrun client
    Signed-off-by: Muvaffak Onus <me@muvaf.com>

--- index.ts
+++ index.ts
@@ -2,7 +2,8 @@
 
 import { getSimulator } from './lib/simulator';
 import { killAllSimulators, simExists } from './lib/utils';
+import { setLimrunIosClient } from 'appium-xcode';
 
-export { getSimulator, killAllSimulators, simExists };
+export { getSimulator, killAllSimulators, simExists, setLimrunIosClient };
 
 export type * from './lib/types';
